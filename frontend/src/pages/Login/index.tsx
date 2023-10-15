5117f6b3dca107cc210eba88d8904cc79485f640
= <vinhphuccla@gmail.com>
2023-10-15 23:05:44 +0700

    Update signup UI

--- frontend/src/pages/Login/index.tsx
+++ frontend/src/pages/Login/index.tsx
@@ -19,50 +19,52 @@ const handleSubmit = () => {
 
 const Login: React.FC = () => {
   return (
-    <div className="container">
-      <h1 className="title">
-        <span>Protect</span> Our Water, <span>Sustain</span> Our Future, <span>Every Drop Matters!</span>
-      </h1>
-      <Formik initialValues={initialValues} onSubmit={handleSubmit} validationSchema={validationSchema}>
-        {(formik) => (
-          <Form className="" onSubmit={formik.handleSubmit}>
-            <h1 className="login__title">LOGIN TO YOUR ACCOUNT</h1>
-            <div className="login__group">
-              <label htmlFor="username" className="">
-                Username
-              </label>
-              <Field
-                id="username"
-                name="username"
-                type="text"
-                placeholder="username"
-                className={`${formik.errors.username && formik.touched.username ? 'border-error' : ''}`}
-              />
-              <ErrorMessage name="username" component="span" className="error-msg" />
-            </div>
-            <div className="login__group">
-              <label htmlFor="password" className="">
-                Password
-              </label>
-              <Field
-                id="password"
-                name="password"
-                type="password"
-                placeholder="password"
-                className={`${formik.errors.password && formik.touched.password ? 'border-error' : ''}`}
-              />
-              <ErrorMessage name="password" component="span" className="error-msg" />
-            </div>
-            <p className="login__forgot">Forgot password?</p>
-            <button type="submit" className="login__btn">
-              LOGIN
-            </button>
-            <div className="login__cta">
-              Don't have an account? <span className="login__signup">Sign up</span>
-            </div>
-          </Form>
-        )}
-      </Formik>
+    <div className="login">
+      <div className="container">
+        <h1 className="slogan">
+          <span>Protect</span> Our Water, <span>Sustain</span> Our Future, <span>Every Drop Matters!</span>
+        </h1>
+        <Formik initialValues={initialValues} onSubmit={handleSubmit} validationSchema={validationSchema}>
+          {(formik) => (
+            <Form className="" onSubmit={formik.handleSubmit}>
+              <h1 className="login__title">LOGIN TO YOUR ACCOUNT</h1>
+              <div className="login__group">
+                <label htmlFor="username" className="">
+                  Username
+                </label>
+                <Field
+                  id="username"
+                  name="username"
+                  type="text"
+                  placeholder="username"
+                  className={`${formik.errors.username && formik.touched.username ? 'border-error' : ''}`}
+                />
+                <ErrorMessage name="username" component="span" className="error-msg" />
+              </div>
+              <div className="login__group">
+                <label htmlFor="password" className="">
+                  Password
+                </label>
+                <Field
+                  id="password"
+                  name="password"
+                  type="password"
+                  placeholder="password"
+                  className={`${formik.errors.password && formik.touched.password ? 'border-error' : ''}`}
+                />
+                <ErrorMessage name="password" component="span" className="error-msg" />
+              </div>
+              <p className="login__forgot">Forgot password?</p>
+              <button type="submit" className="login__btn">
+                LOGIN
+              </button>
+              <div className="login__cta">
+                Don't have an account? <span className="login__signup">Sign up</span>
+              </div>
+            </Form>
+          )}
+        </Formik>
+      </div>
     </div>
   );
 };
--- frontend/src/pages/Sigup/index.tsx
+++ frontend/src/pages/Sigup/index.tsx
@@ -1,7 +1,114 @@
-import React from "react";
+import React from 'react';
+import { ErrorMessage, Field, Form, Formik } from 'formik';
+import * as yup from 'yup';
+import './Signup.scss';
+
+const validationSchema = yup.object({
+  username: yup.string().required('Username is required').trim(),
+  email: yup.string().email('Invalid Email').required('Email is required').trim(),
+  password: yup
+    .string()
+    .required('Password is required')
+    .min(8, 'Weak password')
+    .max(32, 'Password is too long')
+    .trim(),
+  confirmPassword: yup
+    .string()
+    .required('Confirm password is required')
+    .oneOf([yup.ref('password')], 'Confirm password is not match')
+    .trim(),
+});
+
+const initialValues = {
+  username: '',
+  email: '',
+  password: '',
+  confirmPassword: '',
+};
+
+const handleSubmit = () => {
+  console.log('REGISTER SUBMIT');
+};
 
 const Signup: React.FC = () => {
-  return <div>Signup</div>;
+  return (
+    <div className="signup">
+      <div className="container">
+        <h1 className="slogan">
+          <span>Protect</span> Our Water, <span>Sustain</span> Our Future, <span>Every Drop Matters!</span>
+        </h1>
+        <Formik initialValues={initialValues} onSubmit={handleSubmit} validationSchema={validationSchema}>
+          {(formik) => (
+            <Form className="" onSubmit={formik.handleSubmit}>
+              <h1 className="signup__title">SIGN UP YOUR NEW ACCOUNT</h1>
+              {/* USERNAME */}
+              <div className="signup__group">
+                <label htmlFor="username" className="">
+                  Username
+                </label>
+                <Field
+                  id="username"
+                  name="username"
+                  type="text"
+                  placeholder="Username"
+                  className={`${formik.errors.username && formik.touched.username ? 'border-error' : ''}`}
+                />
+                <ErrorMessage name="username" component="span" className="error-msg" />
+              </div>
+              {/* EMAIL */}
+              <div className="signup__group">
+                <label htmlFor="email" className="">
+                  Email
+                </label>
+                <Field
+                  id="email"
+                  name="email"
+                  type="email"
+                  placeholder="Email"
+                  className={`${formik.errors.email && formik.touched.email ? 'border-error' : ''}`}
+                />
+                <ErrorMessage name="email" component="span" className="error-msg" />
+              </div>
+              {/* PASSWORD */}
+              <div className="signup__group">
+                <label htmlFor="password" className="">
+                  Password
+                </label>
+                <Field
+                  id="password"
+                  name="password"
+                  type="password"
+                  placeholder="Password"
+                  className={`${formik.errors.password && formik.touched.password ? 'border-error' : ''}`}
+                />
+                <ErrorMessage name="password" component="span" className="error-msg" />
+              </div>
+              {/* CONFIRM PASSWORD */}
+              <div className="signup__group">
+                <label htmlFor="confirmPassword" className="">
+                  Confirm Password
+                </label>
+                <Field
+                  id="confirmPassword"
+                  name="confirmPassword"
+                  type="password"
+                  placeholder="Confirm Password"
+                  className={`${formik.errors.confirmPassword && formik.touched.confirmPassword ? 'border-error' : ''}`}
+                />
+                <ErrorMessage name="confirmPassword" component="span" className="error-msg" />
+              </div>
+              <button type="submit" className="signup__btn">
+                SIGN UP
+              </button>
+              <div className="signup__cta">
+                Already have an account? <span>Login</span>
+              </div>
+            </Form>
+          )}
+        </Formik>
+      </div>
+    </div>
+  );
 };
 
 export default Signup;
