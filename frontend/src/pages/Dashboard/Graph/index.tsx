af34b4d57f7653f1d37335163592187a320983c5
= <vinhphuccla@gmail.com>
2023-10-16 10:43:09 +0700

    Update Card UI dashboard

---
+++ frontend/src/pages/Dashboard/Graph/index.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const WaterConsumptionGraph: React.FC = () => {
+  return <div>WaterConsumptionGraph</div>;
+};
+
+export default WaterConsumptionGraph;
--- frontend/src/pages/Dashboard/index.tsx
+++ frontend/src/pages/Dashboard/index.tsx
@@ -1,11 +1,46 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './Dashboard.scss';
 import Sidebar from '@/components/Sidebar/Sidebar';
+import Cards from './Card/Cards';
+import { AreaIcon, DeviceIcon, NotificationIcon } from '@/components/Icons';
 
 const Dashboard: React.FC = () => {
+  const [area, setArea] = useState<number>(10);
+  const [devices, setDevices] = useState<number>(20);
+  const [notifications, setNotifications] = useState<number>(30);
+
+  const cardList = [
+    {
+      title: 'AREA',
+      icon: AreaIcon,
+      color: 'green',
+      value: area,
+    },
+    {
+      title: 'DEVICES',
+      icon: DeviceIcon,
+      color: 'yellow',
+      value: devices,
+    },
+    {
+      title: 'NOTIFICATIONS',
+      icon: NotificationIcon,
+      color: 'red',
+      value: notifications,
+    },
+  ];
+
   return (
     <div className="dashboard">
       <Sidebar />
+      <div className="content">
+        <h1 className="title">MY DASHBOARD</h1>
+        <Cards cardList={cardList} />
+        {/* GRAPH */}
+        <div className="graph">
+          <p className="graph__title">The water consumption chart for the area (m3/month)</p>
+        </div>
+      </div>
     </div>
   );
 };
